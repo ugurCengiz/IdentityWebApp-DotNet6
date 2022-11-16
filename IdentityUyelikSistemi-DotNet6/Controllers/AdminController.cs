77854aa222fb39a6279862a1e04f68f846b273e0
uğur cengiz <ugur_nobre@hotmail.com>
2022-11-16 17:37:16 +0300

    16.11.22

---
+++ IdentityUyelikSistemi-DotNet6/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using IdentityUyelikSistemi_DotNet6.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityUyelikSistemi_DotNet6.Controllers
+{
+    public class AdminController : Controller
+    {
+        private UserManager<AppUser> _userManager;
+
+        public AdminController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            
+            return View(_userManager.Users.ToList());
+        }
+    }
+}
